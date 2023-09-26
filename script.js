aaa95a31ba6cd0eec066bd21340f22792c390111
akadwa <kadazh123@gmail.com>
2023-09-26 06:54:44 +0200

    Changed book function to a class

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ let deleteIsActive = false;
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function displayBooks() {
